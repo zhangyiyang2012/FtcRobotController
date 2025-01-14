d44b9a5215fb08672358d3fc89dd8b05c5708bde
zyy <zyy905@outlook.com>
2025-01-14 16:44:06 +0800

    FTC19879张一杨 抓取子系统基础配套程序 配置文件： 上头的“up” 下头的“down” 最大功率：+/-0.6

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawSystem.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ClawSystem {
+    private CRServo up;
+    private CRServo down;
+    private double speed;
+    public ClawSystem(HardwareMap hardwareMap){
+        up = hardwareMap.crservo.get("up");
+        down = hardwareMap.crservo.get("down");
+        speed = 1;
+    }
+
+    private void initServos(){
+        stop();
+    }
+
+    public void stop(){
+        up.setPower(0);
+        down.setPower(0);
+    }
+
+    public void in(){
+        up.setPower(speed);
+        down.setPower(-speed);
+    }
+
+    public void out(){
+        up.setPower(-speed);
+        down.setPower(speed);
+
+    }
+
+    public double getSpeed(){
+        return speed;
+    }
+}
